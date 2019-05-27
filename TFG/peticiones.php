0cab07afd303770cc2141fa714c89a716e6a32c0
Nalone <davidbgh91@gmail.com>
2019-05-27 19:31:39 +0200

    Creación busqueda (Botón de search) y enlace de amistades con sus peticiones

---
+++ TFG/peticiones.php
@@ -0,0 +1,98 @@
+<?php 
+require 'functions/functions.php';
+session_start();
+// Compruebe si el usuario ha iniciado sesión o no
+if (!isset($_SESSION['user_id'])) {
+    header("location:index.php");
+}
+// Establecer conexión de base de datos
+$conn = connect();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>NozzlePrint3D</title>
+    <link rel="stylesheet" type="text/css" href="resources/css/main.css">
+</head>
+<body>
+    <div class="container">
+        <?php include 'includes/navbar.php'; ?>
+        <h1>Peticiones de amistad</h1>
+        <?php
+// Respondiendo a la solicitud
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            if (isset($_GET['accept'])) {
+                $sql = "UPDATE friendship
+                        SET friendship.friendship_status = 1
+                        WHERE friendship.user1_id = {$_GET['id']} AND friendship.user2_id = {$_SESSION['user_id']}";
+                $query = mysqli_query($conn, $sql);
+                if($query){
+                    echo '<div class="userquery">';
+                    echo 'Aceptaste a: ' . $_GET['name'];
+                    echo '<br><br>';
+                    echo 'Redirigiendo la pagina espera 5segundos';
+                    echo '<br><br>';
+                    echo '</div>';
+                    echo '<br>';
+                    header("refresh:5; url=requests.php" );
+                }
+                else{
+                    echo mysqli_error($conn);
+                }
+            } else if(isset($_GET['ignore'])) {
+                $sql6 = "DELETE FROM friendship
+                        WHERE friendship.user1_id = {$_GET['id']} AND friendship.user2_id = {$_SESSION['user_id']}";
+                $query6 = mysqli_query($conn, $sql6);
+                if($query){
+                    echo '<div class="userquery">';
+                    echo 'Ignoraste la petición de: ' . $_GET['name'];
+                    echo '<br><br>';
+                    echo 'Redirigiendo la pagina espera 5segundos';
+                    echo '<br><br>';
+                    echo '</div>';
+                    echo '<br>';
+                    header("refresh:5; url=requests.php" );
+                }
+            }
+        }
+        //
+        ?>
+        <?php
+        $sql = "SELECT users.user_gender, users.user_id, users.user_firstname, users.user_lastname
+                FROM users
+                JOIN friendship
+                ON friendship.user2_id = {$_SESSION['user_id']} AND friendship.friendship_status = 0 AND friendship.user1_id = users.user_id";
+        $query = mysqli_query($conn, $sql);
+        $width = '168px';
+        $height = '168px';
+        if(!$query)
+            echo mysqli_error($conn);
+        if($query){
+            if(mysqli_num_rows($query) == 0){
+                echo '<div class="userquery">';
+                echo 'Parece que  no tienes ninguna petición pendiente';
+                echo '<br><br>';
+                echo '</div>';
+            }
+            while($row = mysqli_fetch_assoc($query)){
+                echo '<div class="userquery">';
+                include 'includes/profile_picture.php';
+                echo '<br>';
+                echo '<a class="profilelink" href="profile.php?id=' . $row['user_id'] .'">' . $row['user_firstname'] . ' ' . $row['user_lastname'] . '<a>';
+                echo '<form method="get" action="requests.php">';
+                echo '<input type="hidden" name="id" value="' . $row['user_id'] . '">';
+                echo '<input type="hidden" name="name" value="' . $row['user_firstname'] . '">';
+                echo '<input type="submit" value="Aceptar" name="accept">';
+                echo '<br><br>';
+                echo '<input type="submit" value="Ignorar" name="ignore">';
+                echo '<br><br>';
+                echo'</form>';
+                echo '</div>';
+                echo '<br>';
+            }
+        }
+        ?>
+    </div>
+</body>
+</html>
